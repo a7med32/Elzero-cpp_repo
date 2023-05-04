433e99f24f4aeb3fc3b0676251606cd4b8a89cdb
ahmed <ahmed01281g@gmail.com>
2023-05-04 15:47:24 +0300

    task file

---
+++ Assignment 1--8/main.cpp
@@ -0,0 +1,39 @@
+//-------First one------//
+// #include <iostream>
+
+// int main()
+// {
+//   std::cout << "Line One/n";
+//   std::cout << "Line Two/n";
+//   std::cout << "Line Three/n";
+//   return 0;
+// }
+//second 
+/*
+  iostream Header File Content
+  -- cout
+  --cin 
+  --cerr
+  --clog
+  --wcout
+  --wcin
+  --wcerr
+  --wclog
+*/
+
+// #include <iostream>
+
+// int main()
+// {
+//   std::cout << "Iam Learning Programming\nWith\nElzero\nWeb\nSchool\n<3";
+  
+//   return 0;
+// }
+#include <iostream>
+
+int main()
+{
+  std::cout << "|=======================|\n|== Elzero Web School ==|\n|==        C++        ==|\n|=======================|";
+  
+  return 0;
+}
