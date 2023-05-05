3de4768da787bfd95de4359cb12b75bee278a05d
ahmed <ahmed01281g@gmail.com>
2023-05-05 15:24:17 +0300

    Second task commit

---
+++ Assignment9-15/main.cpp
@@ -0,0 +1,177 @@
+// ------>first task
+
+#include <iostream>
+using namespace std;
+
+// int main()
+// {
+//   // Write Your Code Here
+//     int age =40;
+//     int period = 10;
+
+//   // Do Not Edit The Next 2 Lines
+//     cout << "My Age Is: " << age << "\n";
+//     cout << period << " Years Ago My Age Was: " << age - period << "\n";
+//     cout << "After "<<period << "my Age will be:"<< age +period<< "\n";
+//   // Add The Third Line Below
+
+//   return 0;
+// }
+
+//---->second task
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//   // Write Your One Line Code Here
+//     int a,b,c;
+//   // Do Not Edit Below
+//     a = 10, b = 20, c = 70;
+//     cout << "Sum Of All Number Is: " << a + b + c;
+//   return 0;
+// }
+//------->third TASK
+    /*
+  name        [ Valid | Good Practice ]
+  NAME        [ Valid | Bad Practice ]
+  1name       [ invalid ] => Reason started with num
+  __name      [ valid ] => Reason good prectice
+  name@name   [ invalid ] => Reason spicial char
+  name10name  [ valid ] => Reason good practice
+  name!name   [ invalid ] => Reason spicial char
+  first_NAME  [ valid ] => Reason Bad practice
+  first_name  [ valid ] => Reason good practice
+  firstName   [ valid ] => Reason good practice
+  first name  [ invalid ] => Reason white space
+  fn          [ valid ] => Reason ?
+  public      [ invalid ] => Reason prohibtid name 
+  Public      [ ??? ] => Reason ?
+*/
+//-------4th task
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//   // Write my_text Variable Content Here
+//   string my_text = "============\n== Elzero ==\n============\n";
+
+//   // Do Not Edit Below
+//   cout << my_text;
+//   return 0;
+// }
+
+// Needed Output
+
+// ============
+// == Elzero ==
+// ============
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//   // Do Not Edit Below
+//   int a, b, c = 100;
+
+//   // Add Your Code Here
+// 	a = b = 100;
+
+//   // Do Not Edit Below
+//   cout << a + b + c; // 300
+//   return 0;
+// }
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//   // Do Not Edit Below
+//   int a = 210, b = -50, c = -40;
+
+//   // Write Your cout Code Here
+// 	cout << a - b - c;
+//   return 0;
+// }
+//----------8
+// #include <iostream>
+// using namespace std;
+
+// int num = 50;
+
+// int read()
+// {
+//   int num = 100;
+//   cout << num;
+//   return 0;
+// }
+
+// int play()
+// {
+//   int num = 200;
+//   read();
+//   return 0;
+// }
+
+// int main()
+// {
+//   play();
+//   return 0;
+// }
+//---------8
+// int main()
+// {
+//   // Edit Here To Fix The Error
+//    int current_year;
+//    int birth_year;
+
+//   // Do Not Edit Here
+//   current_year = 2022;
+//   birth_year = 1982;
+//   int age_in_years = current_year - birth_year;
+//   cout << age_in_years;
+//   return 0;
+// }
+//----------
+// int main()
+// { 
+// 	int x;
+//   // Write Your Code Here
+// 	cout << "enter the num:";
+// 	cin >> x; 
+//   // Num 10
+//   // Needed Output 100 [10 * 10]
+// 	cout << "the Output:"<< x * x;
+//   return 0;
+// }
+//---------
+
+// int main()
+// {
+//   // Write Your Code Here
+// 	int x ,y;
+// 	cout << "enter num1: ";
+// 	cin >> x;
+// 	cout <<"enter num2: ";
+// 	cin >>y;
+// 	cout << "output: "<<y*x;
+//   // Number One => 10
+//   // Number Two => 100
+//   // Needed Output 1000 [10 * 100]
+//   return 0;
+// }
+int main()
+{
+  cout << "Bello\b\b\b\b\bHello Elzeroo\b";
+  cout << " Web Schoolll\b\b";
+  cout << " I Love Programming";
+  cout << " Too Much";
+  cout << " Specially C++\n";
+  return 0;
+}
+
+// Needed Output
+//"Hello Elzero Web School I Love Programming Too Much Specially C++"
+// Hello Elzero Web School I Love Programming Too Much Specially C++
